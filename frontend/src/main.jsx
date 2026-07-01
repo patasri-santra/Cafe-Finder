cf5e2bf69eaecef6d163759b55e1cb0f0bfe5255
patasri-santra <patasrisantra971@gmail.com>
2026-07-01 22:51:21 +0530

    Fix Leaflet marker icons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "leaflet/dist/leaflet.css";
 import "./index.css";
 import App from "./App.jsx";
+
+import "leaflet/dist/leaflet.css";
+
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const root = document.getElementById("root");
 
